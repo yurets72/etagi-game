95c712e79b798e4edcb6b80be5886675aadb6f4f
Yuri <yu.v.devyatkov@gmail.com>
2026-09-17 14:45:04 +0800

    add vite deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,5 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/etagi-game/', // именно так, с именем твоего репо
+})
